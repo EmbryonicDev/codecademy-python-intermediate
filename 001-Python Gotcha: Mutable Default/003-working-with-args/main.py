87414293be515670ed8528d462406759e6733715
djdevlam-apple <embryonic.dev@gmail.com>
2023-03-09 12:54:20 +0100

    working-with-args - complete exercise

--- 001-Python Gotcha: Mutable Default/003-working-with-args/main.py
+++ 001-Python Gotcha: Mutable Default/003-working-with-args/main.py
@@ -20,3 +20,15 @@ def assign_table(table_number, name, vip_status=False):
     tables[table_number]['order'] = ''
 
 # Write your code below:
+
+
+def assign_and_print_order(table_number, *order_items):
+    tables[table_number]['order'] = order_items[:-1]
+
+    for order in order_items:
+        print(order)
+
+
+assign_table(2, 'Arwa', True)
+assign_and_print_order(2, 'Steak', 'Seabass', 'Wine Bottle')
+print(tables)
