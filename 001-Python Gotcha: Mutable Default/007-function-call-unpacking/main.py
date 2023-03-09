db6646d0ea060d586abc814324b838436c402899
djdevlam-apple <embryonic.dev@gmail.com>
2023-03-09 15:39:19 +0100

    Function call unpacking: prepare exercise file

---
+++ 001-Python Gotcha: Mutable Default/007-function-call-unpacking/main.py
@@ -0,0 +1,6 @@
+def calculate_price_per_person(total, tip, split):
+    total_tip = total * (tip/100)
+    split_price = (total + total_tip) / split
+    print(split_price)
+
+# Write your code below:
