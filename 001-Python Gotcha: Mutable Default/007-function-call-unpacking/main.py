b8c52fa65ee76530e95bcc33234ea1c17c614e01
djdevlam-apple <embryonic.dev@gmail.com>
2023-03-09 15:52:36 +0100

    function-call-unpacking: complete exercise

--- 001-Python Gotcha: Mutable Default/007-function-call-unpacking/main.py
+++ 001-Python Gotcha: Mutable Default/007-function-call-unpacking/main.py
@@ -3,4 +3,7 @@ def calculate_price_per_person(total, tip, split):
     split_price = (total + total_tip) / split
     print(split_price)
 
+
 # Write your code below:
+table_7_total = [534.50, 20.0, 5]
+calculate_price_per_person(*table_7_total)
