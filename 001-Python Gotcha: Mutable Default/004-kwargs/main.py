275f528d9299b584ee0cc54c0e198831d8f72d10
djdevlam-apple <embryonic.dev@gmail.com>
2023-03-09 14:35:23 +0100

    kwargs - prepare exercise file

---
+++ 001-Python Gotcha: Mutable Default/004-kwargs/main.py
@@ -0,0 +1,24 @@
+tables = {
+    1: {
+        'name': 'Chioma',
+        'vip_status': False,
+        'order': {
+            'drinks': 'Orange Juice, Apple Juice',
+            'food_items': 'Pancakes'
+        }
+    },
+    2: {},
+    3: {},
+    4: {},
+    5: {},
+    6: {},
+    7: {},
+}
+print(tables)
+
+
+# Write your code below:
+
+
+# Example Call
+# assign_food_items(food='Pancakes, Poached Egg', drinks='Water')
