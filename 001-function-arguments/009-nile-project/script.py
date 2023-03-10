98fae3e46e59621fc886c13df4877e308bb1c077
djdevlam-apple <embryonic.dev@gmail.com>
2023-03-10 16:30:46 +0100

    Nile: complete steps 1 - 8, all related to def calculate_shipping_cost

--- 001-function-arguments/009-nile-project/script.py
+++ 001-function-arguments/009-nile-project/script.py
@@ -4,11 +4,25 @@ from test import test_function
 # Define calculate_shipping_cost() here:
 
 
+def calculate_shipping_cost(from_coords, to_coords, shipping_type='Overnight'):
+    # slow way
+    # from_lat = from_coords[0]
+    # from_long = from_coords[1]
+    # to_lat = to_coords[0]
+    # to_long = to_coords[1]
+    # distance = get_distance(from_lat, from_long, to_lat, to_long)
+
+    # efficient way
+    distance = get_distance(*from_coords, *to_coords)
+    shipping_rate = SHIPPING_PRICES[shipping_type]
+    price = distance*shipping_rate
+    return format_price(price)
+
+
 # Test the function by calling
-# test_function(calculate_shipping_cost)
+test_function(calculate_shipping_cost)
 
 # Define calculate_driver_cost() here
-
 
 # Test the function by calling
 # test_function(calculate_driver_cost)
