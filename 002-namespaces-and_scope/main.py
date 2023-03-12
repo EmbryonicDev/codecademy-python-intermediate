5a78c09ff54bb45b47e95c1f2ca6b2aaf31e4e28
djdevlam-apple <embryonic.dev@gmail.com>
2023-03-12 09:56:22 +0100

    Namespaces / scope: local namespaces: prepare files

---
+++ 002-namespaces-and_scope/main.py
@@ -0,0 +1,23 @@
+global_variable = 'global'
+
+
+print(' -- Local and global Namespaces with empty script -- \n')
+# Write Checkpoint 1 here:
+
+
+# Write Checkpoint 2 here:
+
+
+# Write Checkpoint 3 here:
+
+
+print(' \n -- Local Namespace for divide -- \n')
+# Write Checkpoint 4 here:
+
+
+print(' \n -- Local Namespace for multiply -- \n')
+# Write Checkpoint 5 here:
+
+
+print(' \n -- Local Namespace final -- \n')
+# Write Checkpoint 6 here:
