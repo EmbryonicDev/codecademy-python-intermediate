99f071d4b9516c036fa82eeba787746471cb61b9
djdevlam-apple <embryonic.dev@gmail.com>
2023-03-15 17:03:37 +0100

    OOP: project: refactor code to pass all tests to step 10

--- 004-oop/012-project/main.py
+++ 004-oop/012-project/main.py
@@ -1,5 +1,5 @@
 class School:
-    def __init__(self, name: str, numberOfStudents: int, level: str):
+    def __init__(self, name: str, level: str, numberOfStudents: int):
         self._name = name
         self._level = level
         self._numberOfStudents = numberOfStudents
@@ -21,17 +21,17 @@ class School:
         if isinstance(new_students_added, int):
             self._numberOfStudents = new_students_added
             print((
-                f"{self.name} has {self.numberOfStudents} {'students' if new_students_added > 1 else 'student'}").title())
+                f"{self.name} has {self.numberOfStudents} {'students' if self.numberOfStudents > 1 else 'student'}").title())
         else:
             print("Please enter a Valid Number")
 
     def __repr__(self):
-        return (f"{self.level.title()} school: {self.name}, presently has {self.numberOfStudents} students").title()
+        return (f"{self.level} school: {self.name}, presently has {self.numberOfStudents} {'students' if self.numberOfStudents > 1 else 'student'}").title()
 
 
 class Primary(School):
-    def __init__(self, name, numberOfStudents, level='primary'):
-        School.__init__(self, name, numberOfStudents, level)
+    def __init__(self, name, level, numberOfStudents):
+        super().__init__(name, level, numberOfStudents)
         self._pickupPolicy = "Pick up at 3pm"
 
     @property
@@ -40,13 +40,13 @@ class Primary(School):
 
 
 class Middle(School):
-    def __init__(self, name, numberOfStudents, level='primary'):
-        School.__init__(self, name, numberOfStudents, level)
+    def __init__(self, name, level, numberOfStudents):
+        super().__init__(name, level, numberOfStudents)
 
 
 class High(School):
-    def __init__(self, name, numberOfStudents, level='primary', *sportsTeams):
-        School.__init__(self, name, numberOfStudents, level)
+    def __init__(self, name, level, numberOfStudents, *sportsTeams):
+        super().__init__(name, level, numberOfStudents)
         self._sportsTeams = list(sportsTeams)[0]
 
     @property
@@ -58,23 +58,23 @@ class High(School):
 
 if __name__ == "__main__":
     print(f"\nTesting class School\n")
-    s1 = School('White Horse', 96, 'primary')
+    s1 = School('White Horse', 'primary', 96)
     print(s1)
     s1.numberOfStudents = 13
 
     print(f"\nTesting class Primary\n")
-    p1 = Primary('Primary 1', 64, 'primary')
+    p1 = Primary('Primary 1', 'primary', 64)
     print(p1)
     p1.numberOfStudents = 69
     print(p1.pickupPolicy)
 
     print(f"\nTesting class Middle\n")
-    m1 = Middle('Middle 1', 74, 'middle')
+    m1 = Middle('Middle 1', 'middle', 74)
     print(m1)
     m1.numberOfStudents = 86
 
     print(f"\nTesting class High\n")
-    h1 = High('High 1', 49, 'high', ['basketball', 'tennis'])
+    h1 = High('High 1', 'high', 49, ['basketball', 'tennis'])
     print(h1)
     h1.numberOfStudents = 61
     print(h1.sportsTeams)
