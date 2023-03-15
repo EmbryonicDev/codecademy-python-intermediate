0054f4282945e70f59ae5469756dd86e08850b77
djdevlam-apple <embryonic.dev@gmail.com>
2023-03-15 12:26:33 +0100

    OOP: encapsulation: complete exercise

--- 004-oop/009-encapsulation/main.py
+++ 004-oop/009-encapsulation/main.py
@@ -1,7 +1,9 @@
 class Employee():
     def __init__(self):
         self.id = None
-        # Write your code below
+        # Write your code
+        self._id = 'Whacked'  # use for variable accessible to module
+        self.__id = "Smacked"  # use for variable accessible to class only
 
 
 e = Employee()
