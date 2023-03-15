39df1a6481cd1ea59f0b3c5b1b19fda8e2f46909
djdevlam-apple <embryonic.dev@gmail.com>
2023-03-15 15:09:44 +0100

    OOP: project: Complete class: Primary

--- 004-oop/012-project/main.py
+++ 004-oop/012-project/main.py
@@ -1,5 +1,5 @@
 class School:
-    def __init__(self, name: str, level: str, numberOfStudents: int):
+    def __init__(self, name: str, numberOfStudents: int, level: str):
         self._name = name
         self._level = level
         self._numberOfStudents = numberOfStudents
@@ -29,7 +29,24 @@ class School:
         return (f"{self.level.title()} school: {self.name}, presently has {self.numberOfStudents} students").title()
 
 
+class Primary(School):
+    def __init__(self, name, numberOfStudents, level='primary'):
+        School.__init__(self, name, numberOfStudents, level)
+        self._pickupPolicy = "Pick up at 3pm"
+
+    @property
+    def pickupPolicy(self):
+        return self._pickupPolicy
+
+
 if __name__ == "__main__":
-    s1 = School('White Horse', 'primary', 96)
+    print(f"\nTesting class School\n")
+    s1 = School('White Horse', 96, 'primary')
     print(s1)
     s1.numberOfStudents = 13
+
+    print(f"\nTesting class Primary\n")
+    p1 = Primary('Primary 1', 64, 'primary')
+    print(p1)
+    p1.numberOfStudents = 69
+    print(p1.pickupPolicy)
