468a79dce7a7733afedc6070ded224baba790168
djdevlam-apple <embryonic.dev@gmail.com>
2023-03-17 17:04:43 +0100

    Unit testing: unittest framework: prepare files

--- 005-unit_testing_exceptions/unit_testing/004-unittest_framework/main.py
+++ 005-unit_testing_exceptions/unit_testing/004-unittest_framework/main.py
@@ -1,10 +1,12 @@
+# your code below:
+
 def get_nearest_exit(row_number):
     if row_number < 15:
         location = 'front'
     elif row_number < 30:
         location = 'middle'
     else:
-        location = 'back'
+        location = 'middle'
     return location
 
 # Write your code below:
