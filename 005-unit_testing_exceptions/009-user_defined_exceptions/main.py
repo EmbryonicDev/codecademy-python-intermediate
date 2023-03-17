9a1c485759c1caf19cd7b28951a5294c368da406
djdevlam-apple <embryonic.dev@gmail.com>
2023-03-17 12:51:27 +0100

    Unit Testing exceptions: User-defined Exceptions: complete exercise

--- 005-unit_testing_exceptions/009-user_defined_exceptions/main.py
+++ 005-unit_testing_exceptions/009-user_defined_exceptions/main.py
@@ -6,15 +6,20 @@ inventory = {
 
 
 # Write your code below (Checkpoint 2):
+class InventoryError(Exception):
+    pass
 
 
 def submit_order(instrument, quantity):
     supply = inventory[instrument]
 
     # Write your code below (Checkpoint 3 & 4):
-    inventory[instrument] -= quantity
-    print('Successfully placed order! Remaining supply: ' +
-          str(inventory[instrument]))
+    if quantity > supply:
+        raise InventoryError
+    else:
+        inventory[instrument] -= quantity
+        print('Successfully placed order! Remaining supply: ' +
+              str(inventory[instrument]))
 
 
 instrument = 'Piano'
