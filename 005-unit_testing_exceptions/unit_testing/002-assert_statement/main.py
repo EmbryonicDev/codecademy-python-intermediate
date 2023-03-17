5bda9596c231307935f5aa81283c7697e2850d8d
djdevlam-apple <embryonic.dev@gmail.com>
2023-03-17 16:31:25 +0100

    Unit testing: unit testing: prepare files

--- 005-unit_testing_exceptions/unit_testing/002-assert_statement/main.py
+++ 005-unit_testing_exceptions/unit_testing/002-assert_statement/main.py
@@ -3,12 +3,22 @@ destinations = {
     'CMN': 'Casablanca',
     'IST': 'Istanbul'
 }
+
+
+class AssertionError(Exception):
+    def __init__(self, destination):
+        self.destination = destination
+
+    def __str__(self):
+        return 'Sorry, Small World currently does not fly to this destination!'
+
+
 print('Welcome to Small World Airlines!')
 print('What is the airport code of your travel destination?')
 destination = 'HND'
 
 
 # Write your code below:
+assert destination in destinations, AssertionError(destination)
 
-city_name = destinations[destination]
 print('Great! Retrieving information for your flight to ...' + city_name)
