9582acc0162186f1ed4016f80b78ef4add53fe3c
djdevlam-apple <embryonic.dev@gmail.com>
2023-03-20 11:30:09 +0100

    Unit testing: Assert quantitive methods: complete exercise

--- 005-unit_testing_exceptions/unit_testing/006-assert_quantitave_methods/tests.py
+++ 005-unit_testing_exceptions/unit_testing/006-assert_quantitave_methods/tests.py
@@ -1,23 +1,28 @@
 import unittest
 import entertainment
 
-# Write your code below:
-
 
 class EntertainmentSystemTests(unittest.TestCase):
 
     def test_movie_license(self):
         daily_movie = entertainment.get_daily_movie()
         licensed_movies = entertainment.get_licensed_movies()
-
-        # Checkpoint 2
         self.assertIn(daily_movie, licensed_movies)
 
     def test_wifi_status(self):
         wifi_enabled = entertainment.get_wifi_status()
-
-        # Checkpoint 3
         self.assertTrue(wifi_enabled)
+
+    # Write your code below:
+    # Checkpoint 1
+    def test_maximum_display_brightness(self):
+        brightness = entertainment.get_maximum_display_brightness()
+        self.assertAlmostEqual(brightness,  400)
+
+    # Checkpoint 2
+    def test_device_temperature(self):
+        device_temp = entertainment.get_device_temp()
+        self.assertLess(device_temp, 35)
 
 
 unittest.main()
