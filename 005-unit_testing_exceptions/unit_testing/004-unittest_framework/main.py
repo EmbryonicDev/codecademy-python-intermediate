a3dd1464f68e478544e12f369c5a179455260e9a
djdevlam-apple <embryonic.dev@gmail.com>
2023-03-20 11:13:34 +0100

    Unit testing: Assert Equality and Membership: prepare files

--- 005-unit_testing_exceptions/unit_testing/004-unittest_framework/main.py
+++ 005-unit_testing_exceptions/unit_testing/004-unittest_framework/main.py
@@ -1,4 +1,6 @@
 # your code below:
+import unittest
+
 
 def get_nearest_exit(row_number):
     if row_number < 15:
@@ -6,27 +8,24 @@ def get_nearest_exit(row_number):
     elif row_number < 30:
         location = 'middle'
     else:
-        location = 'middle'
+        location = 'back'
     return location
 
 # Write your code below:
 
 
-def test_row_1():
-    assert get_nearest_exit(
-        1) == 'front', 'The nearest exit to row 1 is in the front!'
+class NearestExitTests(unittest.TestCase):
+    def test_row_1(self):
+        self.assertEqual(get_nearest_exit(1), 'front',
+                         'The nearest exit to row 1 is in the front!')
+
+    def test_row_20(self):
+        self.assertEqual(get_nearest_exit(20), 'middle',
+                         'The nearest exit to row 20 is in the middle!')
+
+    def test_row_40(self):
+        self.assertEqual(get_nearest_exit(40), 'back',
+                         'The nearest exit to row 40 is in the back!')
 
 
-def test_row_20():
-    assert get_nearest_exit(
-        20) == 'middle', 'The nearest exit to row 20 is in the middle!'
-
-
-def test_row_40():
-    assert get_nearest_exit(
-        40) == 'back', 'The nearest exit to row 40 is in the back!'
-
-
-test_row_1()
-test_row_20()
-test_row_40()
+unittest.main()
