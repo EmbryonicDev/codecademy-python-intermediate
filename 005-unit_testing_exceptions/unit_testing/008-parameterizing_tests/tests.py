b52b7c8451b7f7deaa9d1c4dcb6d71ca04e19b26
djdevlam-apple <embryonic.dev@gmail.com>
2023-03-20 15:00:05 +0100

    Unit testing: test fixtures: prepare files

--- 005-unit_testing_exceptions/unit_testing/008-parameterizing_tests/tests.py
+++ 005-unit_testing_exceptions/unit_testing/008-parameterizing_tests/tests.py
@@ -5,12 +5,14 @@ import entertainment
 class EntertainmentSystemTests(unittest.TestCase):
 
     def test_movie_license(self):
-        daily_movie = entertainment.get_daily_movie()
+        daily_movies = entertainment.get_daily_movies()
         licensed_movies = entertainment.get_licensed_movies()
 
         # Write your code below:
-
-        self.assertIn(daily_movie, licensed_movies)
+        for movie in daily_movies:
+            print(movie)
+            with self.subTest(movie):
+                self.assertIn(movie, licensed_movies)
 
 
 unittest.main()
---
+++ 005-unit_testing_exceptions/unit_testing/009-test_fixtures/tests.py
@@ -0,0 +1,16 @@
+import unittest
+import kiosk
+
+
+class CheckInKioskTests(unittest.TestCase):
+
+    def test_check_in_with_flight_number(self):
+        print('Testing the check-in process based on flight number')
+
+    def test_check_in_with_passport(self):
+        print('Testing the check-in process based on passport')
+
+    # Write your code below:
+
+
+unittest.main()
