8651702c88f39e6abaec5894178767a9487006e7
djdevlam-apple <embryonic.dev@gmail.com>
2023-03-20 16:38:07 +0100

    Unit testing: expected failures: complete exercise

--- 005-unit_testing_exceptions/unit_testing/011-expected_failures/tests.py
+++ 005-unit_testing_exceptions/unit_testing/011-expected_failures/tests.py
@@ -5,7 +5,7 @@ import feedback
 class CustomerFeedbackTests(unittest.TestCase):
 
     # Write your code below:
-
+    @unittest.expectedFailure
     def test_survey_form(self):
         self.assertEqual(feedback.issue_survey(), 'Success')
 
