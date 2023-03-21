006a03499d37500fa2ca3a6a1e3544c3987c18cf
djdevlam-apple <embryonic.dev@gmail.com>
2023-03-21 16:07:38 +0100

    Unit testing: project: add comments to all methods in ShoppingCartTests

--- 005-unit_testing_exceptions/unit_testing/013-project/tests.py
+++ 005-unit_testing_exceptions/unit_testing/013-project/tests.py
@@ -7,6 +7,7 @@ class ShoppingCartTests(unittest.TestCase):
     def setUp(self):
         self.cart = surfshop.ShoppingCart()
         self.today = datetime.now()
+        print('New cart created')
 
     def test_add_surfboards_for_multiple_inputs(self):
         for num in [2, 3, 4]:
@@ -16,26 +17,31 @@ class ShoppingCartTests(unittest.TestCase):
                 self.assertEqual(self.cart.add_surfboards(num),
                                  f"Successfully added {num} surfboard{suffix} to cart!")
             self.setUp()
+            print('Tested adding surfboards with qty:', num)
 
     @unittest.skip('Skipping too many boards error')
     def test_too_many_boards_error(self):
         self.assertRaises(surfshop.TooManyBoardsError,
                           self.cart.add_surfboards, 5)
+        print('Tested too many boards error')
 
     # @unittest.expectedFailure
     def test_locals_discount(self):
         self.cart.apply_locals_discount()
         self.assertTrue(self.cart.locals_discount)
+        print('Tested local discount')
 
     def test_past_checkout_date(self):
         past_date = self.today - timedelta(days=3)
         self.assertRaises(surfshop.CheckoutDateError,
                           self.cart.set_checkout_date, past_date)
+        print('Tested past checkout date')
 
     def test_future_checkout_date(self):
         future_date = self.today + timedelta(days=3)
         self.cart.set_checkout_date(future_date)
         self.assertEqual(future_date, self.cart.checkout_date)
+        print('Tested future checkout date')
 
 
 unittest.main()
