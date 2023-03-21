df689ba83aecb5baefdbbd22bc795d1d3801526b
djdevlam-apple <embryonic.dev@gmail.com>
2023-03-21 16:50:07 +0100

    Custom iterators: prepare files

---
+++ 006-iterators_and_generators/iterables_and_iterators/003-custom_iterators/main.py
@@ -0,0 +1,8 @@
+class FishInventory:
+    def __init__(self, fishList):
+        self.available_fish = fishList
+
+
+fish_inventory_cls = FishInventory(["Bubbles", "Finley", "Moby"])
+
+# Write your code below:
