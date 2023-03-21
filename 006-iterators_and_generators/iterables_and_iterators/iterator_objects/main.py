f66d23b311d2af047a0550b67a1cc556e7c69349
djdevlam-apple <embryonic.dev@gmail.com>
2023-03-21 16:31:53 +0100

    Iterators: iterator objects  __iter__() and iter(): complete exercise

--- 006-iterators_and_generators/iterables_and_iterators/iterator_objects/main.py
+++ 006-iterators_and_generators/iterables_and_iterators/iterator_objects/main.py
@@ -1,3 +1,10 @@
 sku_list = [7046538, 8289407, 9056375, 2308597]
 
 # Write your code below:
+print(dir(sku_list))
+sku_iterator_object_one = iter(sku_list)
+sku_iterator_object_two = sku_list.__iter__()
+
+print("-----------------------------")
+print(sku_iterator_object_one)
+print(sku_iterator_object_two)
