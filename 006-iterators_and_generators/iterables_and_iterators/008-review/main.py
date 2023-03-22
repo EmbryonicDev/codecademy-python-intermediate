be1453a992b9fc548d0cbdbf249c54a37e6faa87
djdevlam-apple <embryonic.dev@gmail.com>
2023-03-22 11:33:26 +0100

    Iterables & iterators: review: prepare files

---
+++ 006-iterators_and_generators/iterables_and_iterators/008-review/main.py
@@ -0,0 +1,10 @@
+# Write your code below:
+
+max_money = 15
+options = []
+
+# for combo in toy_combos:
+#    toy1 = combo[0]
+#    cost_of_toy1 = toy1[1]
+#    toy2 = combo[1]
+#    cost_of_toy2 = toy2[1]
