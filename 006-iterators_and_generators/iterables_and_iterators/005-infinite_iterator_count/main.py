d20253e65f0ffe72139804523eca3c85b2a907e5
djdevlam-apple <embryonic.dev@gmail.com>
2023-03-22 11:00:23 +0100

    Infinite Iterator: Count: complete exercise

--- 006-iterators_and_generators/iterables_and_iterators/005-infinite_iterator_count/main.py
+++ 006-iterators_and_generators/iterables_and_iterators/005-infinite_iterator_count/main.py
@@ -1,4 +1,14 @@
 # Write your code below:
 
+import itertools
+
 max_capacity = 1000
 num_bags = 0
+
+for x in itertools.count(start=13.5, step=13.5):
+    if x >= max_capacity:
+        break
+    num_bags += 1
+    print(f"Total bags: {num_bags} = {x}lbs\n")
+
+print(num_bags)
