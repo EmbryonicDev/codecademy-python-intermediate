4505a6b415463e4ee97b93137af329a9ffb5c6ff
djdevlam-apple <embryonic.dev@gmail.com>
2023-03-22 11:30:00 +0100

    Combinatoric Iterator: complete exercise

--- 006-iterators_and_generators/iterables_and_iterators/007-combinatoric_iterator/main.py
+++ 006-iterators_and_generators/iterables_and_iterators/007-combinatoric_iterator/main.py
@@ -3,3 +3,9 @@ import itertools
 collars = ["Red-S", "Red-M", "Blue-XS", "Green-L", "Green-XL", "Yellow-M"]
 
 # Write your code below:
+# Unique combinations of 3 will be created, each in its own tuple
+collar_combo_iterator = itertools.combinations(collars, 3)
+
+# print all unique tuples
+for combo in collar_combo_iterator:
+    print(combo)
