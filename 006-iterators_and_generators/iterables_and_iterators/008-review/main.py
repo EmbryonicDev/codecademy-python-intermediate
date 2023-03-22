7982e590c4b7523f115c5157e1bbca8ca11a4264
djdevlam-apple <embryonic.dev@gmail.com>
2023-03-22 13:16:50 +0100

    Iterables & iterators: review: checkpoint 5

--- 006-iterators_and_generators/iterables_and_iterators/008-review/main.py
+++ 006-iterators_and_generators/iterables_and_iterators/008-review/main.py
@@ -17,8 +17,8 @@ for i in range(len(cat_toys)):
 max_money = 15
 options = []
 
-# for combo in toy_combos:
-#    toy1 = combo[0]
-#    cost_of_toy1 = toy1[1]
+# Checkpoint 5
+toy_combos = itertools.combinations(cat_toys, 2)
+
 #    toy2 = combo[1]
 #    cost_of_toy2 = toy2[1]
