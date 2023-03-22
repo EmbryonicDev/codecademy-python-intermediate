936f207e9af320060858198857e7c54c75aa36ba
djdevlam-apple <embryonic.dev@gmail.com>
2023-03-22 11:16:40 +0100

    Input dependent iterator: complete exercise

--- 006-iterators_and_generators/iterables_and_iterators/006-input__dependent_iterator/main.py
+++ 006-iterators_and_generators/iterables_and_iterators/006-input__dependent_iterator/main.py
@@ -5,3 +5,8 @@ min_pin_pup_foods = [6821904, 3302083]
 pawsome_pup_foods = [9664865]
 
 # Write your code below:
+all_skus_iterator = itertools.chain(
+    great_dane_foods, min_pin_pup_foods, pawsome_pup_foods)
+
+for x in all_skus_iterator:
+    print(x)
