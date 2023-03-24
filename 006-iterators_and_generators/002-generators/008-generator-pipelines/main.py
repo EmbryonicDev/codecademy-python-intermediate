8d3b08395e83c019fdc2f4b34ca66be967d91f05
djdevlam-apple <embryonic.dev@gmail.com>
2023-03-24 15:58:23 +0100

    Generators: pipelines: complete exercise

--- 006-iterators_and_generators/002-generators/008-generator-pipelines/main.py
+++ 006-iterators_and_generators/002-generators/008-generator-pipelines/main.py
@@ -1,3 +1,15 @@
 def course_generator():
     yield ("Computer Science", 5)
     # Write your code below:
+    yield ("Art", 10)
+    yield ("Business", 15)
+
+
+def add_five_students(courses):
+    for course in courses:
+        yield (course[0], course[1]+5)
+
+
+increased_courses = add_five_students(course_generator())
+for course in increased_courses:
+    print(f"{course[0]} with {course[1]+5} students")
