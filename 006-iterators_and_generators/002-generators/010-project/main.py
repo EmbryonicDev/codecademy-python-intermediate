4e919c4d15151e3876599a1cab62a52150e3af28
EmbryonicDev <embryonic.dev@gmail.com>
2023-03-25 20:25:45 +0100

    Generators: project: change checkpoint 1 variable name

--- 006-iterators_and_generators/002-generators/010-project/main.py
+++ 006-iterators_and_generators/002-generators/010-project/main.py
@@ -10,11 +10,11 @@ def read_guestlist(file_name):
             text_file.close()
             break
         name = line_data[0]
-        yield name
         age = int(line_data[1])
         guests[name] = age
+        yield name
 
 # Checkpoint 1   
-guest_list = read_guestlist('guest_list.txt')
+guest_list_generator = read_guestlist('guest_list.txt')
 for i in range(10):
         print(f"{i+1}: {next(guest_list)}")
