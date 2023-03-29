1a223a9ad6d02e38d2563c8712bb59d6633ed1bc
djdevlam-apple <embryonic.dev@gmail.com>
2023-03-29 16:15:33 +0200

    Sets: adding to set: complete exercise

--- 007-sets/003-adding_to_set/main.py
+++ 007-sets/003-adding_to_set/main.py
@@ -5,3 +5,11 @@ user_tag_2 = 'exciting'
 user_tag_3 = 'electric'
 
 # Write your code below!
+# Checkpoint 1
+print('Original song data:', song_data)
+tag_set = set(song_data['Retro Words'])
+# Checkpoint 2
+tag_set.update([user_tag_1, user_tag_2, user_tag_3])
+# Checkpoint 3
+song_data['Retro Words'] = list(tag_set)
+print('New song data:', song_data)
