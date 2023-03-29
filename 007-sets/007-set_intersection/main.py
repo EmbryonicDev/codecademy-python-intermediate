f8c32c88772d1f9c26d5325b803c76f6c361fef0
djdevlam-apple <embryonic.dev@gmail.com>
2023-03-29 18:16:57 +0200

    Sets: set intersection: prepare files

---
+++ 007-sets/007-set_intersection/main.py
@@ -0,0 +1,13 @@
+song_data = {'Retro Words': ['pop', 'warm', 'happy', 'electronic', 'synth'],
+             'Wait For Limit': ['rap', 'upbeat', 'romance'],
+             'Stomping Cue': ['country', 'fiddle', 'party'],
+             'Lowkey Space': ['electronic', 'dance', 'synth', 'upbeat'],
+             'Back To Art': ['pop', 'sad', 'emotional', 'relationship'],
+             'Blinding Era': ['rap', 'intense', 'moving', 'fast'],
+             'Down To Green Hills': ['country', 'relaxing', 'vocal', 'emotional'],
+             'Double Lights': ['electronic', 'chill', 'relaxing', 'piano', 'synth']}
+
+user_recent_songs = {'Retro Words': ['pop', 'warm', 'happy', 'electronic', 'synth'],
+                     'Lowkey Space': ['electronic', 'dance', 'synth', 'upbeat']}
+
+# Write your code below!
