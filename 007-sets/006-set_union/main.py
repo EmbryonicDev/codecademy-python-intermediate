39bdba3f63841d4aa00528342bbc3e2190225c15
djdevlam-apple <embryonic.dev@gmail.com>
2023-03-29 18:15:24 +0200

    Sets: set union: complete exercise

--- 007-sets/006-set_union/main.py
+++ 007-sets/006-set_union/main.py
@@ -9,3 +9,12 @@ user_tag_data = {'Lowkey Space': ['party', 'synth', 'fast', 'upbeat'],
                  'Stomping Cue': ['country', 'swing', 'party', 'instrumental']}
 
 # Write your code below!
+# Checkpoint 1
+new_song_data = {}
+# Checkpoint 2
+# Loop over song_data and create a new set for each category
+for title, tags in song_data.items():
+    new_song_data[title] = set(tags) | set(user_tag_data[title])
+
+# Print the new dictionary
+print(new_song_data)
