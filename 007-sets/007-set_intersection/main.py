8830c0f41b7e1ba19e953d538dd8ad19abcbd7df
djdevlam-apple <embryonic.dev@gmail.com>
2023-03-29 18:46:32 +0200

    Sets: set intersection: complete exercise

--- 007-sets/007-set_intersection/main.py
+++ 007-sets/007-set_intersection/main.py
@@ -1,13 +1,28 @@
-song_data = {'Retro Words': ['pop', 'warm', 'happy', 'electronic', 'synth'],
-             'Wait For Limit': ['rap', 'upbeat', 'romance'],
-             'Stomping Cue': ['country', 'fiddle', 'party'],
-             'Lowkey Space': ['electronic', 'dance', 'synth', 'upbeat'],
-             'Back To Art': ['pop', 'sad', 'emotional', 'relationship'],
-             'Blinding Era': ['rap', 'intense', 'moving', 'fast'],
-             'Down To Green Hills': ['country', 'relaxing', 'vocal', 'emotional'],
-             'Double Lights': ['electronic', 'chill', 'relaxing', 'piano', 'synth']}
+song_data = {
+    'Retro Words': ['pop', 'warm', 'happy', 'electronic', 'synth'],
+    'Wait For Limit': ['rap', 'upbeat', 'romance'],
+    'Stomping Cue': ['country', 'fiddle', 'party'],
+    'Lowkey Space': ['electronic', 'dance', 'synth', 'upbeat'],
+    'Back To Art': ['pop', 'sad', 'emotional', 'relationship'],
+    'Blinding Era': ['rap', 'intense', 'moving', 'fast'],
+    'Down To Green Hills': ['country', 'relaxing', 'vocal', 'emotional'],
+    'Double Lights': ['electronic', 'chill', 'relaxing', 'piano', 'synth']
+}
 
-user_recent_songs = {'Retro Words': ['pop', 'warm', 'happy', 'electronic', 'synth'],
-                     'Lowkey Space': ['electronic', 'dance', 'synth', 'upbeat']}
+user_recent_songs = {
+    'Retro Words': ['pop', 'warm', 'happy', 'electronic', 'synth'],
+    'Lowkey Space': ['electronic', 'dance', 'synth', 'upbeat']
+}
 
-# Write your code below!
+# Step 1: Find intersection of tags for recent songs
+tags_int = set(user_recent_songs['Retro Words']).intersection(
+    set(user_recent_songs['Lowkey Space']))
+print(tags_int)
+
+# Step 2: Find recommended songs based on common tags
+recommended_songs = {}
+for song, tags in song_data.items():
+    if song not in user_recent_songs and set(tags).intersection(tags_int):
+        recommended_songs[song] = tags
+
+print(recommended_songs)
