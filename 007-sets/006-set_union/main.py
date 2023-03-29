281fd406e276f98156ad550d4a6aae93484165b9
djdevlam-apple <embryonic.dev@gmail.com>
2023-03-29 17:51:38 +0200

    Sets: set union: prepare files

---
+++ 007-sets/006-set_union/main.py
@@ -0,0 +1,11 @@
+song_data = {'Retro Words': ['pop', 'warm', 'happy', 'electronic'],
+             'Wait For Limit': ['rap', 'upbeat', 'romance'],
+             'Stomping Cue': ['country', 'fiddle', 'party'],
+             'Lowkey Space': ['electronic', 'dance', 'synth']}
+
+user_tag_data = {'Lowkey Space': ['party', 'synth', 'fast', 'upbeat'],
+                 'Retro Words': ['happy', 'electronic', 'fun', 'exciting'],
+                 'Wait For Limit': ['romance', 'chill', 'rap', 'rhythmic'],
+                 'Stomping Cue': ['country', 'swing', 'party', 'instrumental']}
+
+# Write your code below!
