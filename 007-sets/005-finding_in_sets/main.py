77ac44bee5a6677ba1955a777fbb5a9043735cff
djdevlam-apple <embryonic.dev@gmail.com>
2023-03-29 16:44:53 +0200

    Sets: finding in set: complete exercise

--- 007-sets/005-finding_in_sets/main.py
+++ 007-sets/005-finding_in_sets/main.py
@@ -5,3 +5,22 @@ song_data_users = {'Retro Words': ['pop', 'explosion', 'hammer', 'bomb',
                                    'warm', 'due', 'writer', 'happy', 'horrible', 'electric', 'mushroom', 'shed']}
 
 # Write your code below!
+print(f"\nCheckpoint 1")
+tag_set = set(song_data_users['Retro Words'])
+print(tag_set)
+
+print(f"\nCheckpoint 2")
+bad_tags = []
+for tag in tag_set:
+    if tag not in allowed_tags:
+        bad_tags.append(tag)
+print(bad_tags)
+
+print(f"\nCheckpoint 3")
+print('user tags before removing bad tags:\n', tag_set)
+for tag in bad_tags:
+    tag_set.discard(tag)
+
+print(f"\nCheckpoint 4")
+song_data_users['Retro Words'] = tag_set
+print('user tags after removing bad tags:\n', tag_set)
