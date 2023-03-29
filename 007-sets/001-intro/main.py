97cff6aca70e1ac21bb1c7b11ffa5596f352fb37
djdevlam-apple <embryonic.dev@gmail.com>
2023-03-29 15:57:00 +0200

    Sets: intro: complete exercise

--- 007-sets/001-intro/main.py
+++ 007-sets/001-intro/main.py
@@ -2,3 +2,12 @@ genre_results = ['rap', 'classical', 'rock', 'rock', 'country', 'rap', 'rock', '
                  'rap', 'k-pop', 'rock', 'rap', 'latin', 'pop', 'pop', 'classical', 'pop', 'country', 'rock', 'classical', 'country', 'pop', 'rap', 'latin']
 
 # Write your code below!
+print(f"\nCheckpoint 1")
+survey_genres = set(genre_results)
+for genre in survey_genres:
+    print(genre)
+
+print(f"\nCheckpoint 2")
+survey_abbreviated = set(x[0:3] for x in genre_results)
+for genre in survey_abbreviated:
+    print(genre)
