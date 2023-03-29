ee66ba6e91f195cdfb71efafa04848c50d3b9adc
djdevlam-apple <embryonic.dev@gmail.com>
2023-03-29 16:25:57 +0200

    Sets: remove from set: complete exercise

--- 007-sets/004-removing_from_set/main.py
+++ 007-sets/004-removing_from_set/main.py
@@ -2,3 +2,11 @@ song_data_users = {'Retro Words': [
     'pop', 'onion', 'warm', 'helloworld', 'happy', 'spam', 'electric']}
 
 # Write your code below!
+# Checkpoint 1
+tag_set = set(song_data_users['Retro Words'])
+# Checkpoint 2
+for tag in ['onion', 'helloworld', 'spam']:
+    tag_set.remove(tag)
+# Checkpoint 3
+song_data_users['Retro Words'] = tag_set
+print(song_data_users)
