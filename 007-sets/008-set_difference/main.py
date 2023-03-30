abe816276ce7f89f0fb1646c71e9b8c7b4d8b783
djdevlam-apple <embryonic.dev@gmail.com>
2023-03-30 12:58:06 +0200

    Sets: set difference: prepare files

---
+++ 007-sets/008-set_difference/main.py
@@ -0,0 +1,14 @@
+song_data = {'Retro Words': ['pop', 'warm', 'happy', 'electronic', 'synth'],
+             'Wait For Limit': ['rap', 'upbeat', 'romance', 'relationship'],
+             'Stomping Cue': ['country', 'fiddle', 'party'],
+             'Lowkey Space': ['electronic', 'dance', 'synth', 'upbeat'],
+             'Back To Art': ['pop', 'sad', 'emotional', 'relationship'],
+             'Blinding Era': ['rap', 'intense', 'moving', 'fast'],
+             'Down To Green Hills': ['country', 'relaxing', 'vocal', 'emotional'],
+             'Double Lights': ['electronic', 'chill', 'relaxing', 'piano', 'synth']}
+
+user_liked_song = {'Back To Art': ['pop', 'sad', 'emotional', 'relationship']}
+user_disliked_song = {'Retro Words': [
+    'pop', 'warm', 'happy', 'electronic', 'synth']}
+
+# Write your code below!
