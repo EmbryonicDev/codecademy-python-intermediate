cc5b0a6d777aa3065a8665577a719c1b588a7083
djdevlam-apple <embryonic.dev@gmail.com>
2023-03-30 16:24:51 +0200

    Collections: deque: complete exercise

--- 008-collections/002-deque/main.py
+++ 008-collections/002-deque/main.py
@@ -6,3 +6,25 @@ from collections import deque
 csv_data = process_csv_supplies()[1:]
 
 # Write your code below!
+# Checkpoint 1
+supplies_deque = deque()
+
+print_checkpoint(2)
+for x in csv_data:
+    if x[2] == 'important':
+        supplies_deque.appendleft(x)
+    else:
+        supplies_deque.append(x)
+print('supplies_deque printed:\n', supplies_deque)
+
+print_checkpoint(3)
+ordered_important_supplies = deque()
+for i in range(25):
+    ordered_important_supplies.append(supplies_deque.popleft())
+print('ordered_important_supplies printed:\n', ordered_important_supplies)
+
+print_checkpoint(4)
+ordered_unimportant_supplies = deque()
+for i in range(10):
+    ordered_unimportant_supplies.append(supplies_deque.pop())
+print('ordered_unimportant_supplies printed:\n', ordered_unimportant_supplies)
