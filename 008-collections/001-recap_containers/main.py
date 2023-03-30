aa7e5e0cf764cc5347761d8dc5baf814f0eb27c2
djdevlam-apple <embryonic.dev@gmail.com>
2023-03-30 15:02:41 +0200

    Collections: deque: prepare files

--- 008-collections/001-recap_containers/main.py
+++ 008-collections/001-recap_containers/main.py
@@ -1,1 +1,19 @@
 from print_checkpoint import print_checkpoint
+
+print_checkpoint(1)
+company_name = 'Soul Stitching'
+print('company_name printed:\n', company_name)
+
+print_checkpoint(2)
+company_location = (29.9792458, 31.1342515)
+print('company_location printed:\n', company_location)
+
+print_checkpoint(3)
+company_products = ['shirt', 'trousers', 'shorts', 'hat', 'towel']
+print('company_products printed:\n', company_products)
+
+print_checkpoint(4)
+company_data = {}
+for x in [('name', company_name), ('location', company_location), ('products', company_products)]:
+    company_data[x[0]] = x[1]
+print('company_data printed:\n', company_data)
---
+++ 008-collections/002-deque/main.py
@@ -0,0 +1,8 @@
+from print_checkpoint import *
+from helper_functions import process_csv_supplies
+from collections import deque
+
+# The first row is skipped since it only contains labels
+csv_data = process_csv_supplies()[1:]
+
+# Write your code below!
