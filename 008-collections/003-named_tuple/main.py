e06cbda68fd4f5f05114f5cee5c7c65f0ae851dc
djdevlam-apple <embryonic.dev@gmail.com>
2023-03-30 16:46:52 +0200

    Collections: namedtuple: prepare files

---
+++ 008-collections/003-named_tuple/main.py
@@ -0,0 +1,8 @@
+clothes = [('t-shirt', 'green', 'large', 9.99),
+           ('jeans', 'blue', 'medium', 14.99),
+           ('jacket', 'black', 'x-large', 19.99),
+           ('t-shirt', 'grey', 'small', 8.99),
+           ('shoes', 'white', '12', 24.99),
+           ('t-shirt', 'grey', 'small', 8.99)]
+
+# Write your code below!
