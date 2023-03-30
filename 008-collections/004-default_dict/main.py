1309c9b535de7ff5844ccf846af1623b2db6b702
djdevlam-apple <embryonic.dev@gmail.com>
2023-03-30 16:58:56 +0200

    Collections: default dict: prepare files

---
+++ 008-collections/004-default_dict/main.py
@@ -0,0 +1,15 @@
+from print_checkpoint import *
+
+site_locations = {'t-shirt': 'Shirts',
+                  'dress shirt': 'Shirts',
+                  'flannel shirt': 'Shirts',
+                  'sweatshirt': 'Shirts',
+                  'jeans': 'Pants',
+                  'dress pants': 'Pants',
+                  'cropped pants': 'Pants',
+                  'leggings': 'Pants'
+                  }
+updated_products = ['draped blouse', 'leggings', 'undershirt', 'dress shirt', 'jeans', 'sun dress',
+                    'flannel shirt', 'cropped pants', 'dress pants', 't-shirt', 'camisole top', 'sweatshirt']
+
+# Write your code below!
