a551e3033a5e20f90789b98f6fe4987794683604
djdevlam-apple <embryonic.dev@gmail.com>
2023-03-30 16:47:06 +0200

    Collections: namedtuple: complete exercise

--- 008-collections/003-named_tuple/main.py
+++ 008-collections/003-named_tuple/main.py
@@ -1,3 +1,6 @@
+from print_checkpoint import *
+from collections import namedtuple
+
 clothes = [('t-shirt', 'green', 'large', 9.99),
            ('jeans', 'blue', 'medium', 14.99),
            ('jacket', 'black', 'x-large', 19.99),
@@ -6,3 +9,18 @@ clothes = [('t-shirt', 'green', 'large', 9.99),
            ('t-shirt', 'grey', 'small', 8.99)]
 
 # Write your code below!
+# Checkpoint 1
+ClothingItem = namedtuple('ClothingItem', ['type', 'color', 'size', 'price'])
+
+# Checkpoint 2
+new_coat = ClothingItem('coat', 'black', 'small', 14.99)
+
+# Checkpoint 3
+coat_cost = new_coat.price
+
+print_checkpoint(4)
+updated_clothes_data = []
+for item in clothes:
+    updated_clothes_data.append(ClothingItem(
+        item[0], item[1], item[2], item[3]))
+print('updated_clothes_data printed:\n', updated_clothes_data)
