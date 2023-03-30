a8e9fe03e2fe89952057d28c31a72ad027a6aecc
djdevlam-apple <embryonic.dev@gmail.com>
2023-03-30 17:19:21 +0200

    Collections: default dict: complete exercise

--- 008-collections/004-default_dict/main.py
+++ 008-collections/004-default_dict/main.py
@@ -1,4 +1,5 @@
 from print_checkpoint import *
+from collections import defaultdict
 
 site_locations = {'t-shirt': 'Shirts',
                   'dress shirt': 'Shirts',
@@ -13,3 +14,14 @@ updated_products = ['draped blouse', 'leggings', 'undershirt', 'dress shirt', 'j
                     'flannel shirt', 'cropped pants', 'dress pants', 't-shirt', 'camisole top', 'sweatshirt']
 
 # Write your code below!
+# Checkpoint 1
+validated_locations = defaultdict(lambda: 'TODO: Add to website')
+
+# Checkpoint 2
+validated_locations.update(site_locations)
+
+print_checkpoint(3)
+for product in updated_products:
+    if product not in validated_locations:
+        site_locations[product] = validated_locations[product]
+print(site_locations)
