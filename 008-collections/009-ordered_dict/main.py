22460b01e5b649cd460bf217cc9fe5d4cb17e55d
djdevlam-apple <embryonic.dev@gmail.com>
2023-03-30 18:19:03 +0200

    Collections: ordered dict: complete exercise

--- 008-collections/009-ordered_dict/main.py
+++ 008-collections/009-ordered_dict/main.py
@@ -19,3 +19,29 @@ order_data = [['Order: 1', 'purchased'],
               ['Order: 15', 'purchased']]
 
 # Write your code below!
+# Checkpoint 1
+orders = OrderedDict(order_data)
+
+# Checkpoint 2
+print_checkpoint(2)
+to_move = []
+to_remove = []
+for order, status in orders.items():
+    if status == 'returned':
+        to_move.append(order)
+    elif status == 'canceled':
+        to_remove.append(order)
+print('to_move printed:\n', to_move)
+print('\nto_remove printed:\n', to_remove)
+
+# Checkpoint 3
+print_checkpoint(3)
+for order in to_remove:
+    orders.pop(order)
+print('orders printed after removing cancelled items:\n', orders)
+
+# Checkpoint 4
+print_checkpoint(4)
+for order in to_move:
+    orders.move_to_end(order)
+print('orders printed after moving returned items to end:\n', orders)
