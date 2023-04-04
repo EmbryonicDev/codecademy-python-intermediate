6da62a4d821951a38b5bfc2f0845cbea00c321e0
djdevlam-apple <embryonic.dev@gmail.com>
2023-04-04 11:30:53 +0200

    Context managers: class based: complete exercise

--- 009-context_managers/002-class_based/main.py
+++ 009-context_managers/002-class_based/main.py
@@ -1,1 +1,13 @@
-from print_checkpoint import *
+class PoemFiles:
+    def __init__(self):
+        print('Creating Poems!')
+
+    def __enter__(self):
+        print('Opening poem file')
+
+    def __exit__(self, *exc):
+        print('Closing poem file')
+
+
+with PoemFiles() as manager:
+    print('Hope is the thing with feathers')
