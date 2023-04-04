a7c5571c7e4b7a02ad9304cdafead4ea1219afe5
djdevlam-apple <embryonic.dev@gmail.com>
2023-04-04 11:00:10 +0200

    Context managers: with statement: prepare files

---
+++ 009-context_managers/001-with_statement/main.py
@@ -0,0 +1,2 @@
+open_file = open('file_name.txt', 'r')
+print(open_file.read())
---
+++ 009-context_managers/001-with_statement/print_checkpoint.py
@@ -0,0 +1,4 @@
+def print_checkpoint(checkpoint: int):
+    string = f'\nCheckpoint {checkpoint}'
+    decor = '-'*len(string)
+    print(string, f"\n{decor}")
