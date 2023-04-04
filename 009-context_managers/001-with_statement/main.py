1314997d8adbfab38ae892d675780a1d7aba68da
djdevlam-apple <embryonic.dev@gmail.com>
2023-04-04 11:09:53 +0200

    Context managers: with statement: complete exercise

--- 009-context_managers/001-with_statement/main.py
+++ 009-context_managers/001-with_statement/main.py
@@ -1,2 +1,12 @@
-open_file = open('file_name.txt', 'r')
-print(open_file.read())
+from print_checkpoint import *
+
+print_checkpoint(1)
+try:
+    open_file = open('file_name.txt', 'r')
+    print(open_file.read())
+finally:
+    open_file.close()
+
+print_checkpoint(2)
+with open('file_name.txt', 'r') as open_file:
+    print(open_file.read())
